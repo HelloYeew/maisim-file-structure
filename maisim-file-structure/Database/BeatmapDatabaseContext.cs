932069356f33c4edaf1324dc9cd0c23b6840ca02
Phawit Pornwattanakul <me@helloyeew.dev>
2022-04-17 03:23:27 +0700

    Add EFCore to test it with current structure

---
+++ maisim-file-structure/Database/BeatmapDatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using maisim_file_structure.Objects;
+using Microsoft.EntityFrameworkCore;
+
+namespace maisim_file_structure.Database;
+
+public class MaisimDatabaseContext : DbContext
+{
+    public DbSet<Beatmap> Beatmaps { get; set; }
+    public DbSet<BeatmapSet> BeatmapSets { get; set; }
+    
+    public string DbPath { get; set; }
+
+    public MaisimDatabaseContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "beatmaps.db");
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Filename={DbPath}");
+    }
+}
